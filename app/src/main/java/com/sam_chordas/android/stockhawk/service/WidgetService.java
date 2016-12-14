bbea1f9e08a82cda181b5e1b2625750e613cdd51
Sushant Mathur <sushant.post@gmail.com>
2016-12-14 22:34:52 +0530

    Added RTL support tag in Manifest.

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/WidgetService.java
@@ -0,0 +1,98 @@
+package com.sam_chordas.android.stockhawk.service;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteDatabase;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+import static com.sam_chordas.android.stockhawk.data.QuoteProvider.AUTHORITY;
+
+/**
+ * Created by sushant on 11/12/16.
+ */
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext(),intent);
+    }
+    class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+        private Context mContext;
+        private Cursor mCursor;
+        private int mAppWidgetId;
+
+        public StackRemoteViewsFactory(Context context, Intent intent) {
+            mContext = context;
+            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+            if (mCursor != null) {
+                mCursor.close();
+            }
+
+            Uri uri = Uri.parse("content://" + AUTHORITY);
+            String projection[] =
+                    {QuoteColumns._ID,
+                    QuoteColumns.BIDPRICE,
+                    QuoteColumns.CHANGE};
+                     mCursor = mContext.getContentResolver().query(uri, null, null,
+                    null, null);
+        }
+
+        @Override
+        public void onDestroy() {
+            if (mCursor != null) {
+                mCursor.close();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            return null;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+    }
+}
+
+
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockWidgetProvider.java
@@ -1,16 +1,19 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.rest.QuoteCursorAdapter;
+import com.sam_chordas.android.stockhawk.service.StockIntentService;
 
 /**
  * Created by sushant on 11/11/16.
@@ -26,12 +29,14 @@ public class StockWidgetProvider extends AppWidgetProvider {
             QuoteCursorAdapter adapter = new QuoteCursorAdapter(context,null);
 
             //Code to open the activity when opened
-            RemoteViews remoteView = new RemoteViews(context.getPackageName(),R.id.widget_list);
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(),R.layout.widget_layout);
             Intent intent = new Intent(context,MyStocksActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
             remoteView.setOnClickPendingIntent(R.id.WidgetList,pendingIntent);
 
+            //Code to Display the stocks
 
+            remoteView.setRemoteAdapter(R.id.widget_list,intent);
         }
     }
 
@@ -40,6 +45,13 @@ public class StockWidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);
         String action = intent.getAction();
         Log.i("Receiver", "Broadcast received: " + action);
+        if(action.equals(MyStocksActivity.ACTION_DATA_UPDATED)){
+            //Query for real data and post it to the ListView
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.widget_layout);
+            //Intent myPopulatingIntent = new Intent();
+            Intent stockIntent = new Intent(context,StockIntentService.class);
+            remoteViews.setRemoteAdapter(R.id.widget_list,stockIntent);
 
+        }
     }
 }
